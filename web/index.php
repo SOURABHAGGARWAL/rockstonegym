4e2b79f22e960494df31102f2e870cac7e0b0f54
Sourabh Aggarwal <saggarwa@beachbody.com>
2015-03-19 16:31:49 +0530

    remove all

--- web/index.php
+++ web/index.php
@@ -16,23 +16,23 @@ if ($page_name=='') {
 elseif ($page_name=='index.html') {
 	include $browser_t.'/index.html';
 	}
-elseif ($page_name=='commercial.html') {
-	include $browser_t.'/commercial.html';
+elseif ($page_name=='about.html') {
+	include $browser_t.'/about.html';
 	}
-elseif ($page_name=='industrial.html') {
-	include $browser_t.'/industrial.html';
+elseif ($page_name=='service.html') {
+	include $browser_t.'/service.html';
 	}
-elseif ($page_name=='infrastructure.html') {
-	include $browser_t.'/infrastructure.html';
-	}
-elseif ($page_name=='resource.html') {
-	include $browser_t.'/resource.html';
+elseif ($page_name=='blog.html') {
+	include $browser_t.'/blog.html';
 	}
 elseif ($page_name=='contact.html') {
 	include $browser_t.'/contact.html';
 	}
 elseif ($page_name=='datails.html') {
 	include $browser_t.'/details.html';
+	}
+elseif ($page_name=='404.html') {
+	include $browser_t.'/404.html';
 	}
 elseif ($page_name=='contact-post.html') {
 	include 'app/contact.php';
