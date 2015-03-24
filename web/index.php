ca2dd23af226b2ec29e36482870f19e018b8ce53
Sourabh Aggarwal <saggarwa@beachbody.com>
2015-03-24 14:22:16 +0530

    changes

--- web/index.php
+++ web/index.php
@@ -5,33 +5,32 @@ include "app/detect.php";
 if ($page_name=='') {
 	include $browser_t.'/index.html';
 	}
-elseif ($page_name=='index.html') {
+elseif ($page_name=='index.php?page=index') {
 	include $browser_t.'/index.html';
 	}
-elseif ($page_name=='about.html') {
+elseif ($page_name=='index.php?page=blog') {
 	include $browser_t.'/about.html';
 	}
-elseif ($page_name=='service.html') {
+elseif ($page_name=='index.php?page=service') {
 	include $browser_t.'/service.html';
 	}
-elseif ($page_name=='blog.html') {
+elseif ($page_name=='index.php?page=blog') {
 	include $browser_t.'/blog.html';
 	}
-elseif ($page_name=='contact.html') {
+elseif ($page_name=='index.php?page=contact') {
 	include $browser_t.'/contact.html';
 	}
-elseif ($page_name=='datails.html') {
+elseif ($page_name=='index.php?page=details') {
 	include $browser_t.'/details.html';
 	}
-elseif ($page_name=='404.html') {
+elseif ($page_name=='index.php?page=details') {
 	include $browser_t.'/404.html';
 	}
-elseif ($page_name=='contact-post.html') {
+elseif ($page_name=='index.php?page=contact-post') {
 	include 'app/contact.php';
 	}
-else
-	{
-		include $browser_t.'/404.html';
+else{
+	include $browser_t.'/index.html';
 	}
 
 ?>
